4ec36878a777c12e62c9481385b2aa882d487b85
cesarOchoa90 <hire.cesar@outlook.com>
2025-01-30 22:17:37 -0600

    debugged mysql

--- controllers/savedlists.js
+++ controllers/savedlists.js
@@ -1,45 +1,48 @@
-const pool = require('../mysql/connection')
-const mysql = require("mysql")
-const connection = require("../mysql/connection")
+const pool = require("../mysql/connection");
+const mysql = require("mysql");
+const connection = require("../mysql/connection");
 
 const getSavedLists = (req, res) => {
-    connection.query("SELECT * FROM Saved Properties", (err, rows) => {
+  connection.query("SELECT * FROM Saved Properties", (err, rows) => {
+    if (err) {
+      console.log({ message: "Error occurred: " + err });
+      return res.status(500).send("An unexpected error occurred");
+    }
+    res.json(rows);
+  });
+};
+
+const getSavedListbyId = (req, res) => {
+  const SavedListbyId = req.params.id;
+  const replacements = [SavedListbyId];
+  connection.query(
+    "SELECT * FROM Saved Properties WHERE ID = ?",
+    replacements,
+    (err, rows) => {
       if (err) {
-        console.log({ message: "Error occurred: " + err });
+        console.log({ message: "Error occured:" + err });
         return res.status(500).send("An unexpected error occurred");
       }
-      res.json(rows)
-    });
-  }
+      res.json(rows);
+    }
+  );
+};
 
-  const getSavedListbyId = (req, res) =>{
-    const SavedListbyId = req.params.id
-    const replacements = [SavedListbyId]
-    connection.query("SELECT * FROM Saved Properties WHERE ID = ?" , replacements,
+const createSavedList = (req, res) => {
+  console.log("inside create saved item");
+  const { PropertyId, UserId } = req.body;
+  const replacements = [UserId, PropertyId];
+  connection.query(
+    "INSERT INTO `Saved Properties` (UserId, PropertyId ) VALUES (?,?)",
+    replacements,
     (err, rows) => {
-        if(err) {
-          console.log({message: "Error occured:" + err});
-          return res.status(500).send("An unexpected error occurred")
-        }
-        res.json(rows);
-      } )
-  }
+      if (err) {
+        console.log({ message: "Error occured:" + err });
+        return res.status(500).send("An unexpected error occurred");
+      }
+      res.json(rows);
+    }
+  );
+};
 
-  const createSavedList = (req, res) =>{
-    console.log('inside create saved item')
-    const { PropertyId, UserId } = req.body
-    const replacements = [UserId, PropertyId]
-    connection.query("INSERT INTO `Saved Properties` (UserId, PropertyId ) VALUES (?,?)" , replacements,  (err, rows) => {
-        if(err) {
-          console.log({message: "Error occured:" + err});
-          return res.status(500).send("An unexpected error occurred")
-        }
-        res.json(rows);
-      } )
-
-
-  }
-
-  
-
-  module.exports = {getSavedLists, getSavedListbyId, createSavedList}
+module.exports = { getSavedLists, getSavedListbyId, createSavedList };
